5817277c6a00c232a9dac2ba86a6ca0716561127
Putri Larasati <putrilarasati440@gmail.com>
2022-03-19 10:28:38 +0700

    menambahkan class Penjumlahan dengan method

--- modul4_1302204011/modul4_1302204011/Program.cs
+++ modul4_1302204011/modul4_1302204011/Program.cs
@@ -10,6 +10,18 @@ namespace modul4_1302204011
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<float>(13, 02, 20));
+        }
+    class Penjumlahan
+        {
+            public static T JumlahTigaAngka<T> (T input1, T input2, T input3)
+            {
+                dynamic tempA = input1;
+                dynamic tempB = input2;
+                dynamic tempC = input3;
+
+                return tempA + tempB + tempC;
+            }
         }
     }
 }
